34784b89bc9c46e829acc7a2536f82b572c1164c
Shreyash Gawande <shreyashmgawande@gmail.com>
2025-12-09 23:16:41 +0530

    Fix formatting issues in MyConnection.java

--- MyConnection.java
+++ MyConnection.java
@@ -8,7 +8,7 @@ public class MyConnection {
 		try {
 			
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/company?useSSL=false", "root", "root@2002");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/company?useSSL=false","root", "root@2002");
 		}
 		catch(Exception ex)
 		{
@@ -17,3 +17,4 @@ public class MyConnection {
 	}
 
 }
+
