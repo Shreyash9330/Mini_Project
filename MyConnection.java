e1cd2f6becfbc840dde14b433285327b057ea8ce
Shreyash Gawande <shreyashmgawande@gmail.com>
2025-12-14 17:40:56 +0530

    Update MyConnection.java

--- MyConnection.java
+++ MyConnection.java
@@ -5,16 +5,17 @@ public class MyConnection {
 		public Connection con;
 	MyConnection()
 	{
-		try {
-			
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/company?useSSL=false","root", "root@2002");
-		}
-		catch(Exception ex)
-		{
-			System.out.println(ex);
-		}
+			try 
+			{
+					Class.forName("com.mysql.cj.jdbc.Driver");
+					con = DriverManager.getConnection("jdbc:mysql://localhost:3306/company?useSSL=false","root", "root@2002");
+			}
+			catch(Exception ex)
+			{
+					System.out.println(ex);
+			}
 	}
 
 }
+
 
